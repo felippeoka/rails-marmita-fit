0dfa007bf922f69714081c578fd50f1bb0f4a819
Felippe Oka <felippe.oka@gmail.com>
2022-10-22 17:47:40 -0300

    Inicio da index

--- app/models/lunchbox.rb
+++ app/models/lunchbox.rb
@@ -1,5 +1,3 @@
 class Lunchbox < ApplicationRecord
   belongs_to :user
-
-  validates :food, :size, :price, :presence => true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,4 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :lunchbox
-
-  validates :user_id, :presence => true, uniqueness: { scope: :user_id}
-  validates :lunchbox_id, :presence => true, uniqueness: { scope: :lunchbox_id}
 end
