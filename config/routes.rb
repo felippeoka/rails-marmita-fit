aebb2d5da6692b521cde272308a5d3a0f21e73b4
Andre Gorgen <andregorgen@gmail.com>
2022-10-29 14:41:00 -0300

    purchase page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: "lunchboxes#index"
 
   resources :lunchboxes
+
+  get 'purchase', to: 'purchases#purchase'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
